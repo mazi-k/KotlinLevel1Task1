1dd2a18c29c3342ab68b9ed758b65a3d5d4a1322
maria <podliza3000@gmail.com>
2023-10-25 15:56:20 +0300

    simple square adding

--- app/src/main/java/com/example/kotlinlevel1task1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlevel1task1/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.kotlinlevel1task1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinlevel1task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(KEY_COUNT, 0)
+            setData()
+        }
+
+        binding.addButton.setOnClickListener {
+            updateData()
+        }
+    }
+
+    @Override
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNT, counter)
+    }
+
+    private fun setData() {
+        binding.counter.text = counter.toString()
+        for (i in (1..counter)) {
+            val frame = ScrollView(this)
+            layoutInflater.inflate(R.layout.square_item, frame)
+            binding.gridLayout.addView(frame)
+            val square = frame.findViewById<Button>(R.id.square)
+            square.text = i.toString()
+        }
+    }
+
+    private fun updateData() {
+        counter++
+        binding.counter.text = counter.toString()
+        val frame = ScrollView(this)
+        layoutInflater.inflate(R.layout.square_item, frame)
+        binding.gridLayout.addView(frame)
+        val square = frame.findViewById<Button>(R.id.square)
+        square.text = counter.toString()
+
+    }
+
+    companion object {
+        private const val KEY_COUNT = "COUNT"
     }
 }
